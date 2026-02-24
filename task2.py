a01640425643b4a131bdd2dd281b0b0be1575ff2
SushantPatil04 <patilsushant9767@gmail.com>
2026-02-24 10:03:25 +0530

    task 2 is complted

---
+++ task2.py
@@ -0,0 +1,12 @@
+#Using the Math Module for Calculations
+
+import math
+
+n = int(input("enter a number :"))
+square_root = math.sqrt(n)
+logarithm = math.log(n)
+sine = math.sin(n)
+
+print(f" square root : {square_root}")
+print(f" logarithm :{logarithm}")
+print(f" sine  : {sine}")
