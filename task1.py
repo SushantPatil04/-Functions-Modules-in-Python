41d48d35927d98a8adc9a0b1b57b3d1fad94e369
SushantPatil04 <patilsushant9767@gmail.com>
2026-02-24 10:02:46 +0530

    task 1 is complted

---
+++ task1.py
@@ -0,0 +1,10 @@
+#Calculate Factorial Using a Function
+
+n = int(input("enter a number :"))
+def factorial(n):
+    if n == 1:
+        return 1
+    else :
+        return n * factorial(n-1)
+
+print(f"factorial of {n} is {factorial(n)}")
